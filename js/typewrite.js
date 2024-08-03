9c4c5a843a83d5b3dc4f5cf40f99d283910533c4
kuuyyaa <146645814+kuuyyaa@users.noreply.github.com>
2024-08-03 21:25:20 +0100

    quick links

--- js/typewrite.js
+++ js/typewrite.js
@@ -31,7 +31,7 @@ TxtType.prototype.tick = function() {
     } else if (this.isDeleting && this.txt === '') {
     this.isDeleting = false;
     this.loopNum++;
-    delta = 50;
+    delta = 5;
     }
 
     setTimeout(function() {
